fb7e1a95bd898338e7b58cd55073267d8135e821
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-07-20 23:56:30 -0300

    adiciona configs em jest.config.js

--- src/app/services/UserService.spec.ts
+++ src/app/services/UserService.spec.ts
@@ -1,3 +1,11 @@
-test('should return sum two numbers', () => {
-    expect(1 + 2).toBe(3);
+import UserService from './UserService';
+
+describe('User', () => {
+    it('should to be able to create a new user', () => {
+        expect(1 + 2).toBe(3);
+    })
+    
+    it('should not be able to create two user with the same email', () => {
+        expect(1 + 2).toBe(3);
+    })
 })
