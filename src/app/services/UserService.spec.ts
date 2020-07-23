e4f977912c9e5f9589b2d72c5a38944d7ad27f1e
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-07-23 13:09:06 -0300

    adiciona mais infos em anotacoes.txt sobre o jest

--- src/app/services/UserService.spec.ts
+++ src/app/services/UserService.spec.ts
@@ -1,11 +1,25 @@
 import UserService from './UserService';
+// import * as bcrypt from "bcryptjs";
 
 describe('User', () => {
-    it('should to be able to create a new user', () => {
+    it('should to be able to create a new user', async  () => {
+        
+        // const userService = new UserService();
         expect(1 + 2).toBe(3);
+
+        // const user = await userService.create({
+        //     email: 'q@q.com',
+        //     password: 'q',
+        //     commission: '0',
+        //     name: 'q',
+        //     phone: 'qq9999999'
+        // })
+
+        // expect(user).toHaveProperty('id');
+        // expect(user.password).toBe(await bcrypt.hash(user.password, 8));
     })
     
-    it('should not be able to create two user with the same email', () => {
-        expect(1 + 2).toBe(3);
-    })
+    // it('should not be able to create two user with the same email', () => {
+    //     expect(1 + 2).toBe(3);
+    // })
 })
